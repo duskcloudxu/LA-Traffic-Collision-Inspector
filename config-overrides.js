42fcedf87070d2136b8641d4079d256c0a76a04c
TheDusKnight <860956020@qq.com>
2019-10-27 19:49:18 -0700

    add all sorting and filtering function

--- config-overrides.js
+++ config-overrides.js
@@ -8,7 +8,7 @@ module.exports = override(
     }),
     addLessLoader({
         javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#2AA719', '@secondary-color': '#A0D911', '@font-size-base': '14px',
+        modifyVars: { '@primary-color': '#25b864', '@secondary-color': '#A0D911', '@font-size-base': '14px',
             '@heading-color': 'rgba(0, 0, 0, 0.85)', '@text-color': 'rgba(0, 0, 0, 0.65)',
             '@text-color-secondary': 'rgba(0, 0, 0, .45)', '@border-radius-base': '4px',
             '@border-color-base': '#d9d9d9', '@buttonFace': '#5B83AD',
